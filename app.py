e1dc1cd09578594ae44e8c16e28dd58ae398b642
Nikodemus HS <101912576+nikodemushs@users.noreply.github.com>
2025-06-24 14:39:08 +0700

    update remove req

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-# app.py  ← only the parts that changed / were added
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle, os
-from sklearn.pipeline import Pipeline
 
 app = Flask(__name__)
 CORS(app)
